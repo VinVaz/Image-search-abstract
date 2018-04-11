172c6c76871143fe7a878dc70a01331edacffa59
VinVaz <viniciusvazpro@gmail.com>
2018-04-11 19:22:29 -0300

    add functionalities

--- ImageSearch.js
+++ ImageSearch.js
@@ -2,11 +2,16 @@ const http = require('http');
 const MongoClient = require("mongodb").MongoClient;
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 var port = process.env.PORT || 8080;
 
 http.createServer(function(req, res){
 	
-	var response = [];
+	var url = req.url;
+	var offset = "";
+	var termreq = "";
+	
+
 	
 	var returnedQuery = {
 		url: "a",
@@ -14,11 +19,23 @@ http.createServer(function(req, res){
 		thumbnail: "c",
 		context: "d"
 	}
+	var _term = url.parse(termreq);
 	
+	
+	MongoClient.update({}, {}, function(date){
+		
+	});
+    var response = [];
 	var recentSearchQueries = {
 		term: "A",
 		when: "B",
 	}
+	for(var i = 0; i < 10, i++){
+		
+	}
+	res.writeHead('Content-Type':'text/plain');
+	res.write(JSON.parse(response));
+	res.end();
 	
 }).listen(port);
 
