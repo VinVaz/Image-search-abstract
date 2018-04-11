859fe53f5af158677dce6e9716dd04edf8ed6813
VinVaz <viniciusvazpro@gmail.com>
2018-04-11 18:45:20 -0300

    add reponse objects

--- ImageSearch.js
+++ ImageSearch.js
@@ -4,6 +4,24 @@ const fs = require('fs');
 const path = require('path');
 var port = process.env.PORT || 8080;
 
+http.createServer(function(req, res){
+	
+	var response = [];
+	
+	var returnedQuery = {
+		url: "a",
+		snippet: "b",
+		thumbnail: "c",
+		context: "d"
+	}
+	
+	var recentSearchQueries = {
+		term: "A",
+		when: "B",
+	}
+	
+}).listen(port);
+
 
 
 
