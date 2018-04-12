f6ee00a93bae220ae3c8858b2fc7042ada420dcc
VinVaz <viniciusvazpro@gmail.com>
2018-04-12 09:12:27 -0300

    add restriction to what url are permited

--- ImageSearch.js
+++ ImageSearch.js
@@ -7,42 +7,76 @@ var port = process.env.PORT || 8080;
 
 const dbUrl = 'mongodb://localhost:27017';
 const dbName = "test";
-const dbCollectionName = "searchs";
+const dbCollectionName = "searches";
 
 
 http.createServer(function(req, res){
 	
 	var timeOfRequest = new Date();
 	var parsedUrl = url.parse(req.url);
-	var pathName = parsedUrl.pathname;
-	var myOffset = parsedUrl.query;
-	var urlBaseName = path.basename(pathName);
-	var urlDirName = path.dirname(pathName);
-	var _term = decodeURIComponent(urlBaseName);
-	
+	var urlPathName = parsedUrl.pathname;
+	var urlQuery = parsedUrl.query;
+	var urlDirName = path.dirname(urlPathName);
+	var urlBaseName = "";
+	var clientSearch = "";	  
+	var recentSearchQueries = {};
+
+
 	var returnedQuery = {
 		url: "a",
 		snippet: "b",
 		thumbnail: "c",
 		context: "d"
 	}
-	console.log(req.url);
-	var recentSearchQueries = {
-		term: _term,
-		when: timeOfRequest
-	}
+	//have to validate the urlBaseName later
+	//have to create validation test to the myOffset later
 	
-	MongoClient.connect(dbUrl, function(err, client){
+	if(urlDirName=="/api/imagesearch"){
+		
+	  var urlBaseName = path.basename(urlPathName);
+	  var clientSearch = decodeURIComponent(urlBaseName); 
+	  var recentSearchQueries = {
+		term: clientSearch,
+		when: timeOfRequest
+	  }	  
+		
+	  MongoClient.connect(dbUrl, function(err, client){
 		if(err) console.log("err");
-		var db = client.db(dbName);
-		var collection = db.collection(dbCollectionName);
+		const db = client.db(dbName);
+		const collection = db.collection(dbCollectionName);
 		collection.insertOne(recentSearchQueries);
 		client.close();
-	});
-	
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.write(JSON.stringify(recentSearchQueries));
-	res.end();
+	  });
+	  
+	  //will return the searched images
+	  res.writeHead(200, {'Content-Type':'text/plain'});
+	  res.write(JSON.stringify(recentSearchQueries));
+	  res.end();
+	}
+	if(urlDirName=="/api/latest"){
+		MongoClient.connect(dbUrl, function(err, client){
+		if(err) console.log("err");
+		const db = client.db(dbName);
+		const collection = db.collection(dbCollectionName);
+        
+		function response(){
+		    collection.find({}, function(err, data){
+			callback(null, data);
+		  });
+		}
+		response(function(err, data){
+			res.writeHead(200, {'Content-Type':'text/plain'});
+			res.write(JSON.stringify(data));
+			res.end();
+		});
+		client.close();
+	  });
+	}	
+    else{
+      res.writeHead(200, {'Content-Type':'text/plain'});
+	  res.write("404: Page Not Found");
+	  res.end();
+	}
 	
 }).listen(port);
 
